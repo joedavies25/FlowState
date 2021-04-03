90ee05c808b7d8b9038d32bb9c75e502f940ec22
joedavies25 <joedavies2500@gmail.com>
2021-04-03 09:40:52 +0100

    feat: readme

--- flow-state/client/__test__/global-mock-fetch.js
+++
@@ -1,4 +0,0 @@
-global.fetch = require('jest-fetch-mock');
-
-jest.mock('@expo/vector-icons'); // to avoid errors for Icon rendering
-jest.mock('react-native/Libraries/Animated/src/NativeAnimatedHelper'); // to avoid warnings about NativeAnimatedHelp
